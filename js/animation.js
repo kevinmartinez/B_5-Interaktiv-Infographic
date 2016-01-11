f0a543baa2235f25525b385a80e3a6c363d314ea
Kevin <kevin@pixelkojan.se>
2016-01-11 14:40:47 +0100

    add h2 animation jQuery

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+// Fade animation för mina h2
+$(window).scroll(function() {
+  $('.is-hidden').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+700) {
+      $(this).addClass("fadeIn");
+    }
+  });
+});
